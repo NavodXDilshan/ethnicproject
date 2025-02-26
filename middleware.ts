e8dd0207c96b0e81fd395ef69e7a41f22bbb6170
NavodXDilshan <en97569@foe.sjp.ac.lk>
2025-02-26 23:55:18 +0530

    Runtime Key resolve

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-    matcher: "/protected-route", // Adjust based on where you use middleware
-    runtime: "nodejs", // ✅ Ensures it runs in Node.js instead of Edge
+
 };
 
 export function middleware(req: NextRequest) {
